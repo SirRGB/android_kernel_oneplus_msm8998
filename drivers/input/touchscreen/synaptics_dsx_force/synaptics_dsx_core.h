b4a2088bc8292489c12afc7d65183385a57eded5
SirRGB <sirrgb@proton.me>
2026-07-09 22:19:25 +0200

    Revert "synaptics_dsx_force: guard test reporting"
    This reverts commit 8830adeeb6fc2d6053520e553d9895876ea61528.

--- drivers/input/touchscreen/synaptics_dsx_force/synaptics_dsx_core.h
+++ drivers/input/touchscreen/synaptics_dsx_force/synaptics_dsx_core.h
@@ -405,12 +405,8 @@ struct synaptics_rmi4_data {
 	bool chip_is_tddi;
 	bool open_test_b7;
 	bool short_test_extend;
-
-#ifdef CONFIG_TOUCHSCREEN_SYNAPTICS_DSX_TEST_REPORTING_FORCE
 	bool disable_data_dump;
 	bool dump_flags;
-#endif
-
 	bool enable_reversed_keys;
 
 	int (*reset_device)(struct synaptics_rmi4_data *rmi4_data,
@@ -426,9 +422,7 @@ struct synaptics_rmi4_data {
 	struct pinctrl_state *pinctrl_state_suspend;
 	struct synaptics_dsx_factory_param *factory_param;
 
-#ifdef CONFIG_TOUCHSCREEN_SYNAPTICS_DSX_TEST_REPORTING_FORCE
 	struct completion dump_completion;
-#endif
 #ifdef CONFIG_TOUCH_DEBUG_FS
 	struct dentry *debugfs;
 #endif
